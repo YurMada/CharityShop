1247e76d3964f2dd8d44bf0f3e477d5aee5e42d0
Yuriks1 <89460670+Yuriks1@users.noreply.github.com>
2022-03-09 13:30:51 +0100

    Create more entities

---
+++ src/main/java/java/se/iths/charity_shop/entity/DonationEntity.java
@@ -0,0 +1,24 @@
+package java.se.iths.charity_shop.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class DonationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}
