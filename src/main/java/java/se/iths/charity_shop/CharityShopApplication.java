fbe3ea879a6621d8f479efd9a9692612deec93a0
Yuriks1 <89460670+Yuriks1@users.noreply.github.com>
2022-03-09 12:11:01 +0100

    Add files

---
+++ src/main/java/java/se/iths/charity_shop/CharityShopApplication.java
@@ -0,0 +1,13 @@
+package java.se.iths.charity_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/se/iths/charity_shop/CharityShopApplicationTests.java
@@ -0,0 +1,13 @@
+package java.se.iths.charity_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
