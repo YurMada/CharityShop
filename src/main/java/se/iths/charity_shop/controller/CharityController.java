7bfd341b7fb41d0bb9399fec2484f17c0f3eab77
Yuriks1 <89460670+Yuriks1@users.noreply.github.com>
2022-03-12 11:23:35 +0100

    Add Charity

---
+++ src/main/java/se/iths/charity_shop/controller/CharityController.java
@@ -0,0 +1,45 @@
+package se.iths.charity_shop.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.charity_shop.entity.CharityEntity;
+import se.iths.charity_shop.service.CharityService;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("charities")
+
+public class CharityController {
+
+    CharityService charityService;
+    public CharityController(CharityService charityService){
+        this.charityService = charityService;}
+
+    @PostMapping
+    public ResponseEntity<CharityEntity> createCharity(@RequestBody CharityEntity charityEntity){
+        CharityEntity createdCharity = charityService.createCharity(charityEntity);
+        return new ResponseEntity(createdCharity, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCharity(@PathVariable Long id){
+        charityService.deleteCharity(id);
+    }
+
+
+    @GetMapping("{id}")
+    public Optional<CharityEntity> findCharityById(@PathVariable Long id){
+        return charityService.findCharityById(id);
+
+    }
+
+    @GetMapping
+    public Iterable<CharityEntity> findAll(){
+        return charityService.findAll();
+
+    }
+}
+
---
+++ src/main/java/se/iths/charity_shop/service/CharityService.java
@@ -0,0 +1,34 @@
+package se.iths.charity_shop.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.charity_shop.entity.CharityEntity;
+import se.iths.charity_shop.repository.CharityRepository;
+import java.util.Optional;
+
+@Service
+public class CharityService {
+
+    CharityRepository charityRepository;
+
+    public CharityService(CharityRepository charityRepository) {
+        this.charityRepository = charityRepository;
+    }
+
+    public CharityEntity createCharity(CharityEntity charityEntity) {
+        return charityRepository.save(charityEntity);
+    }
+
+    public void deleteCharity(Long id) {
+        Optional<CharityEntity> foundCharity = charityRepository.findById(id);
+        charityRepository.deleteById(id);
+    }
+
+    public Optional<CharityEntity> findCharityById(Long id) {
+        return charityRepository.findById(id);
+    }
+
+    public Iterable<CharityEntity> findAll() {
+        return charityRepository.findAll();
+    }
+
+}
