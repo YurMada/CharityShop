48c024c68a33a0003eb514e0168016d1830357a1
Yuriks1 <89460670+Yuriks1@users.noreply.github.com>
2022-03-12 18:18:01 +0100

    Add Employee

---
+++ src/main/java/se/iths/charity_shop/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package se.iths.charity_shop.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.charity_shop.entity.EmployeeEntity;
+import se.iths.charity_shop.repository.EmployeeRepository;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    EmployeeRepository employeeRepository;
+
+    public EmployeeService( EmployeeRepository employeeRepository) {this.employeeRepository = employeeRepository;}
+
+    public EmployeeEntity createEmployee(EmployeeEntity employeeEntity) {return employeeRepository.save(employeeEntity);}
+
+    public void deleteEmployee(Long id) {
+        Optional<EmployeeEntity> foundEmployee = employeeRepository.findById(id);
+        employeeRepository.deleteById(id);
+    }
+
+    public Optional<EmployeeEntity> findEmployeeById(Long id) {return employeeRepository.findById(id);}
+
+    public Iterable<EmployeeEntity> findAll() {return employeeRepository.findAll();}
+}
