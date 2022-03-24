0c0c5067f4bb8922905b9e65c2433a954cce28f2
Madelene <madelene.axblom@gmail.com>
2022-03-24 08:48:17 +0100

    Add JMS Integration RabbitMQ  using the spring-amqp library

---
+++ src/main/java/se/iths/charity_shop/message/MessageConfig.java
@@ -0,0 +1,28 @@
+package se.iths.charity_shop.message;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"message"})
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public Queue massage() {
+        return new Queue("message");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public Receiver receiver() {
+        return new Receiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public Sender sender() {
+        return new Sender();
+    }
+}
