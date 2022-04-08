ff931da2dcb95c0b21d55462b9e7e2d11857642e
Yuriks1 <juris.adijans@iths.se>
2022-04-08 11:04:13 +0200

    Add more tests for Charity/Donation

--- src/test/java/se/iths/charity_shop/controller/StockControllerTest.java
+++ src/test/java/se/iths/charity_shop/controller/StockControllerTest.java
@@ -8,23 +8,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import se.iths.charity_shop.entity.StockEntity;
 import se.iths.charity_shop.repository.StockRepository;
 import se.iths.charity_shop.service.StockService;
-
-import static groovy.json.JsonOutput.toJson;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.RequestEntity.post;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Ignore
 @RunWith(SpringRunner.class)
@@ -37,7 +28,7 @@ public class StockControllerTest {
     @InjectMocks
     StockService stockService;
    @Test
-    public void createStock_whenPostMethod() throws Exception {
+    public void createStock_whenPostMethod() {
         StockEntity stock = new StockEntity();
         stock.setAmount(1000.00);
         when(stockRepository.save(ArgumentMatchers.any(StockEntity.class))).thenReturn(stock);
@@ -47,5 +38,7 @@ public class StockControllerTest {
 
         verify(stockRepository).save(stock);
     }
+
+
 }
 
